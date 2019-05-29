abc6749f434eaf3f12719a54fa4aee002b291fc8
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2019-05-30 01:41:32 +0530

    Create Java If-Else.java

---
+++ Java If-Else.java
@@ -0,0 +1,21 @@
+/*Code provided by hakerarenatutorials.wordpress.com*/
+import java.util.*;
+public class Solution 
+{
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) 
+    {
+        int N = scanner.nextInt();
+        if(N%2!=0)
+            System.out.println("Weird");
+        else
+        {
+            if(N>=2&&N<=5)
+                System.out.println("Not Weird");
+            else if(N>=6&&N<=20)
+                System.out.println("Weird");
+            else if(N>=20)
+                System.out.println("Not Weird");
+        }
+    }
+}
