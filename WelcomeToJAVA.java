4faade06d6e1c122168a8676f224c13a0802250d
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2019-05-30 01:21:50 +0530

    Create WelcomeToJAVA.java

---
+++ WelcomeToJAVA.java
@@ -0,0 +1,8 @@
+/*Code provided by hakerarenatutorials.wordpress.com*/
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World.\nHello, Java.");
+    }
+}
