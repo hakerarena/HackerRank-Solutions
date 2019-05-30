704c8134b0fe7c1fdfae2fed192d124364da8a7c
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2019-05-30 18:20:10 +0530

    Create Java BigInteger .java

---
+++ Java BigInteger .java
@@ -0,0 +1,17 @@
+/*Code provided by hakerarenatutorials.wordpress.com*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        BigInteger a=sc.nextBigInteger(),b=sc.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
+
