4aaeebed4d2900f59539f37d907156e9fe8cd75e
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2020-04-14 02:24:38 +0530

    Create Tuples.py

---
+++ Python/Tuples.py
@@ -0,0 +1,5 @@
+/*Code provided by hakerarenatutorials.wordpress.com*/
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    print(hash(tuple(integer_list)))
