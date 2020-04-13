2f6023f85a026b5b92c483c47a733aa7c566a8e9
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2020-04-14 02:12:58 +0530

    Create Loops.py

---
+++ Python/Loops.py
@@ -0,0 +1,8 @@
+/*Code provided by hakerarenatutorials.wordpress.com*/
+if __name__ == '__main__':
+    n = int(input())
+    k=1;x=0;
+    for i in range(n):
+        print(x)
+        x=x+k
+        k+=2
