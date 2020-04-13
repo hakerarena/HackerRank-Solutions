6d098f32e6520322a595c9d458b4e77c37d06306
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2020-04-14 02:32:46 +0530

    Create Set .symmetric_difference() Operation.py

---
+++ Python/Set .symmetric_difference() Operation.py
@@ -0,0 +1,6 @@
+ /*Code provided by hakerarenatutorials.wordpress.com*/
+size1=int(input())
+s1=set(map(int,input().split()))
+size2=int(input())
+s2=set(map(int,input().split()))
+print(len(s1.symmetric_difference(s2)))
